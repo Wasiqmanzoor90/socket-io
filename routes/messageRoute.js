7317c4f91adea274562afc1bc6381826de875540
Wasiq Manzoor <164636793+Wasiqmanzoor90@users.noreply.github.com>
2025-07-16 22:45:09 +0530

    m2

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -2,10 +2,10 @@ import express from 'express';
 import authMiddleware from '../middleware/auth.middleware.js';
 import { createMessage, getRoomMessages } from '../controller/messageController.js';
 
+//routes
 const router = express.Router();
 
 router.use(authMiddleware);
-
 router.post('/', createMessage);
 router.get('/:roomId', getRoomMessages);
 
