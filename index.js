588fe119671ce909d3b226b24078126bc67d83a3
Wasiq Manzoor <164636793+Wasiqmanzoor90@users.noreply.github.com>
2025-07-16 23:03:32 +0530

    m3

--- index.js
+++ index.js
@@ -3,28 +3,52 @@ import dotenv from 'dotenv';
 import ConnectDb from './config/dbConnect.js';
 import cors from 'cors';
 import authRoute from './routes/authRoute.js';
+import http from 'http'; 
 import messageRoute from './routes/messageRoute.js';
+import { Server } from 'socket.io'; // Import Server from socket.io
+import socketHandler from './socket/socketHandler.js';
 
 
 // Suppress dotenv logs
 dotenv.config({ quiet: true });
 
-const server = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Create HTTP server manually instead of using app.listen
+const server = http.createServer(app);
+
+//create a socket.io server
+const io = new Server(server,{
+  cors: {
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  
+  }
+})
+
+
+
+
 
 //connet to the database
 ConnectDb();
 
 
 // Middleware
-server.use(express.json());
-server.use(cors());
+app.use(express.json());
+app.use(cors());
 
 
 
 //Routes
-server.use('/api/auth', authRoute);
-server.use('/api/message', messageRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/message', messageRoute);
+
+
+// Socket.io handler
+socketHandler(io); // Initialize socket handler
+
 
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
