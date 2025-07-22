ca8a6f8677b7b5094ec0154a1a11e24f5fbece7c
Wasiq Manzoor <164636793+Wasiqmanzoor90@users.noreply.github.com>
2025-07-23 00:43:12 +0530

    mk7

--- backend/my-app/controller/authController.js
+++ backend/my-app/controller/authController.js
@@ -26,13 +26,20 @@ const register = async (req, res) => {
     const { name, email, password } = req.body;
     try {
         if (!name || !email || !password) {
-        return res.status(400).json({ message: "Please provide all required fields" });
+            return res.status(400).json({ 
+                success: false,
+                message: "Please provide all required fields" 
+            });
         }
 
         const existUser = await User.findOne({ email });
         if (existUser) {
-            return res.status(400).json({ message: "User already exists" });
+            return res.status(400).json({ 
+                success: false,
+                message: "User already exists" 
+            });
         }
+        
         const hashPass = await bcrypt.hash(password, 10);
 
         const user = await User.create({
@@ -40,10 +47,20 @@ const register = async (req, res) => {
             email,
             password: hashPass
         });
+        
         await user.save();
-        res.status(201).json({ message: "User registered successfully", user });
+        
+        res.status(201).json({ 
+            success: true,
+            message: "User registered successfully", 
+            user 
+        });
     } catch (error) {
-        return res.status(500).json({ message: 'Server error', error: error.message });
+        return res.status(500).json({ 
+            success: false,
+            message: 'Server error', 
+            error: error.message 
+        });
     }
 }
 
